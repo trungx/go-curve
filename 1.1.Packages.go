b8e3f4eb6263783f04efb0322329492035498a0e
Trung Hong Ta <trung.ta@hdwebsoft.co>
2019-01-21 09:32:23 +0700

    .

---
+++ 1.1.Packages.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" //https://golang.org/pkg/fmt/
+
+func main() {
+    fmt.Println("Lại là Hello World")
+}
---
+++ 1.2.Variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt" //https://golang.org/pkg/fmt/
+
+func test() {
+	// Khai báo biến message có kiểu dữ liệu string
+	var message string
+
+	// Khai báo 3 biến c, python, java đều có kiểu dữ liệu là bool
+	var c, python, java bool
+
+	// Khai báo 2 biến i, j có kiểu dữ liệu là int và khởi tạo luôn giá trị cho chúng
+	var i, j int = 1, 2
+
+	// Khai báo ngắn gọn biến k và khởi tạo giá trị luôn cho nó.
+	// Không dùng từ khóa var mà dùng dấu hai chấm, lúc này kiểu dữ liệu sẽ được ngầm định tùy theo giá trị của biến.
+	k := 3
+    fmt.Println("Lại là Hello World",k,c,python,java,message,i,j,k)
+}
+
